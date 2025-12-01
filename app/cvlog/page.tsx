2bdf6047c73f0281b4dd6c89c3e2a3c6a2ced1b0
Mathieu Franchimont <franchimont.mathieu@gmail.com>
2025-12-01 11:51:09 +0100

    bug fixes with database : lacking exporting and importing

--- app/cvlog/page.tsx
+++ app/cvlog/page.tsx
@@ -7,7 +7,7 @@ import { headers } from 'next/headers'
 import { redirect } from 'next/navigation'
 
 export async function addBlock(form: FormData) {
-  await db.insert(cvBlocksTable).values({
+  db.insert(cvBlocksTable).values({
     title: String(form.get('title')),
     timeline: String(form.get('timeline')),
     description: String(form.get('description')),
@@ -63,13 +63,13 @@ export default async function CVLOG() {
                     required
                     ></textarea>
                 </div>
-            </form>
-            <button
+                <button
                 type="submit"
                 className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-blue-700 sm:w-fit hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
                 Submit
             </button>
+            </form>
         </div>
     </section>
     );
--- db/schema.ts
+++ db/schema.ts
@@ -1,7 +1,8 @@
+
 import { boolean, pgTable, text, uuid } from 'drizzle-orm/pg-core'
 
 export const cvBlocksTable = pgTable('cv_blocks', {
-  id: uuid().defaultRandom().primaryKey(),   // ➜ ajoute un id !
+  id: uuid().defaultRandom().primaryKey(),
   title: text().notNull(),
   timeline: text().notNull(),
   description: text().notNull(),
